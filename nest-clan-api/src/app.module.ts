30b6dc66676dcbec2f1c2339d287d64c52c83df2
Davi Paluch <davipaluch@icloud.com>
2025-02-03 22:53:28 -0300

    feat: config JWT

--- nest-clan-api/src/app.module.ts
+++ nest-clan-api/src/app.module.ts
@@ -3,15 +3,18 @@ import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      // É utilizado o método '.forRoot' caso o modulo necessite de configuração, caso não necessite use apenas ConfigModule
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
