79c76cc8ec22b37312cd40c2978d3bc6c6d69cd8
davipaluch <davipaluch@icloud.com>
2025-06-18 20:35:39 -0300

    feat: documentation

--- nest-clan-api/src/infra/main.ts
+++ nest-clan-api/src/infra/main.ts
@@ -2,11 +2,32 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { Env } from './env'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   })
+
+  const config = new DocumentBuilder()
+    .setTitle('Forum API')
+    .setDescription(
+      'API de um fórum de perguntas e respostas com suporte a comentários, anexos e notificações.\n\n⚠️ Esta "DOCUMENTAÇÃO" é apenas para fins de demonstração e **não representa o projeto real**.',
+    )
+    .setVersion('1.0')
+    .addTag(
+      'Perguntas',
+      'Gerenciamento de perguntas (criar, editar, excluir, buscar, etc).',
+    )
+    .addTag('Respostas', 'Gerenciamento de respostas às perguntas.')
+    .addTag('Comentários', 'Comentários em perguntas e respostas.')
+    .addTag('Anexos', 'Gerenciamento de arquivos anexados.')
+    .addTag('Notificações', 'Notificações e eventos do sistema.')
+    .addTag('Eventos de Domínio', 'Eventos que disparam ações assíncronas.')
+    .build()
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
 
   const configService: ConfigService<Env, true> = app.get(ConfigService)
   const port = configService.get('PORT', { infer: true })
